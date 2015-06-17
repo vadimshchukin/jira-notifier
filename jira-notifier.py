17568aa570597424591fa01c72d4e840c7d2f5ce
jericho <hexuniverse@gmail.com>
2015-06-17 15:03:23 +0500

    structure improved

--- jira-notifier.py
+++ jira-notifier.py
@@ -16,10 +16,10 @@ import webbrowser
 
 import gtk # GTK bindings
 import gobject # GObject library
-import appindicator # GNOME indicators
+import appindicator # libappindicator bindings
 import pynotify # NotifyOSD notifications
 import feedparser # RSS/Atom feed parser
-import pyquery # jQuery-like library for Python
+import pyquery # jQuery-like library
 
 class Flusher(): # standard output flusher
     def __init__(self, file):
@@ -29,31 +29,11 @@ class Flusher(): # standard output flusher
         self.file.write(data) # write data as ususal
         self.file.flush() # force flush
 
-class Application: # application logic
-    def __init__(self):
-        sys.stdout = Flusher(sys.stdout) # force standard output flushing
-        pynotify.init('jira-notifier') # initialize NotifyOSD notifications
-        self.published = {} # projects activity streams last published timestamps
+class JIRA:
+    def __init__(self, trackerURL):
+        self.trackerURL = trackerURL
         # create URL opener using cookie jar
         self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
-        self.update = True # update loop flag
-
-    def initialize(self):
-        # create GNOME indicator
-        iconFileName = os.path.abspath(self.iconFileName)
-        category = appindicator.CATEGORY_APPLICATION_STATUS
-        self.indicator = appindicator.Indicator('jira-notifier', iconFileName, category)
-        self.indicator.set_status(appindicator.STATUS_ACTIVE) # set initial active status
-
-        exitItem = gtk.MenuItem('Exit') # create exit item
-        exitItem.connect('activate', gtk.main_quit) # connect with GTK main loop quit
-        indicatorMenu = gtk.Menu() # create indicator menu
-        indicatorMenu.append(exitItem) # append it
-        indicatorMenu.show_all() # show all items and menu itself
-        self.indicator.set_menu(indicatorMenu) # set indicator menu
-
-        if not os.path.isdir(self.avatarsDirectoryName): # if avatars directory doesn't exist
-            os.mkdir(self.avatarsDirectoryName) # then create it
 
     def login(self, username, password): # log in JIRA
         request = urllib2.Request(self.trackerURL + '/rest/auth/latest/session') # construct request URL
@@ -74,6 +54,49 @@ class Application: # application logic
         webFile = self.opener.open(request) # perform request and open output stream
         webFile.close()
 
+    def getActivityStream(self, projectKey):
+        parameters = urllib.urlencode({'streams': 'key IS ' + projectKey}) # prepare GET parameteres
+        request = urllib2.Request(self.trackerURL + '/activity?' + parameters) # construct request
+        webFile = self.opener.open(request) # perform request and open output stream
+        feed = feedparser.parse(webFile.read()) # parse Atom feed
+        webFile.close() # close output stream
+        return feed
+
+    def searchIssues(self, searchQuery):
+        parameters = urllib.urlencode({'jql': searchQuery}) # prepare GET parameteres
+        resourceURL = self.trackerURL + '/rest/api/2/search?' + parameters # request issues by JQL filter
+        webFile = self.opener.open(urllib2.Request(resourceURL)) # perform request and open output stream
+        response = json.loads(webFile.read()) # parse response JSON
+        webFile.close() # close output stream
+        return response
+
+class Application: # application logic
+    def __init__(self):
+        sys.stdout = Flusher(sys.stdout) # force standard output flushing
+        pynotify.init('jira-notifier') # initialize NotifyOSD notifications
+        self.published = {} # projects activity streams last published timestamps
+        # create URL opener using cookie jar
+        self.update = True # update loop flag
+
+    def initialize(self):
+        # create GNOME indicator
+        iconFileName = os.path.abspath(self.iconFileName)
+        category = appindicator.CATEGORY_APPLICATION_STATUS
+        self.indicator = appindicator.Indicator('jira-notifier', iconFileName, category)
+        self.indicator.set_status(appindicator.STATUS_ACTIVE) # set initial active status
+
+        exitItem = gtk.MenuItem('Exit') # create exit item
+        exitItem.connect('activate', gtk.main_quit) # connect with GTK main loop quit
+        indicatorMenu = gtk.Menu() # create indicator menu
+        indicatorMenu.append(exitItem) # append it
+        indicatorMenu.show_all() # show all items and menu itself
+        self.indicator.set_menu(indicatorMenu) # set indicator menu
+
+        if not os.path.isdir(self.avatarsDirectoryName): # if avatars directory doesn't exist
+            os.mkdir(self.avatarsDirectoryName) # then create it
+
+        self.clientJIRA = JIRA(self.trackerURL) # create JIRA client
+
     def handleMenuItemSelect(self, widget, issueURL): # indicator menu item select handler
         output = os.dup(1) # duplicate standard otuput
         os.close(1) # close standard output
@@ -85,12 +108,8 @@ class Application: # application logic
             os.dup2(output, 1) # duplicate original standard output back
 
     def processProjectActivityStream(self, projectKey): # process Atom feed
-        # construct request
-        request = urllib2.Request(self.trackerURL + '/activity?streams=key+IS+%s' % projectKey)
-        webFile = self.opener.open(request) # perform request and open output stream
-        feed = feedparser.parse(webFile.read()) # parse Atom feed
+        feed = self.clientJIRA.getActivityStream(projectKey)
         feedEntries = feed['entries'] # get feed entries
-        webFile.close() # close output stream
 
         feedEntries.reverse() # reverse entries order
         for feedEntry in feedEntries: # loop through activity stream entries
@@ -134,11 +153,7 @@ class Application: # application logic
                 self.published[projectKey] = feedEntry['published_parsed'] # update projects last published timestamps
 
     def processIssueQuery(self, issueQuery, menu):
-        parameters = urllib.urlencode({'jql': issueQuery}) # prepare GET parameteres
-        resourceURL = self.trackerURL + '/rest/api/2/search?%s' % parameters # request issues by JQL filter
-        webFile = self.opener.open(urllib2.Request(resourceURL)) # perform request and open output stream
-        response = json.loads(webFile.read()) # parse response JSON
-        webFile.close() # close output stream
+        response = self.clientJIRA.searchIssues(issueQuery)
 
         projectKey = None # current project key
         for issue in response['issues']: # loop through response issues
@@ -184,28 +199,29 @@ class Application: # application logic
     def updateDataLoop(self): # infinite update loop
         if self.username:
             print "logging in JIRA as '%s'" % self.username # print log message
-            self.login(self.username, self.password) # log in jira
+            self.clientJIRA.login(self.username, self.password) # log in jira
 
+        print 'starting update loop'
         while self.update:
             self.updateData() # update JIRA data
-            print 'waiting for %d seconds' % self.updateInterval
+            print 'waiting %d seconds' % self.updateInterval
             time.sleep(self.updateInterval) # suspend execution for a specified interval
         
         if self.username:
             print 'logging out JIRA' # print log message
-            self.logout() # log out JIRA
+            self.clientJIRA.logout() # log out JIRA
 
-    def run(self): # main method
-        # construct argument parser
+    def run(self): # main function
+        # create argument parser
         argumentParser = argparse.ArgumentParser(description='Desktop notifier for JIRA issue tracker')
-        argumentParser.add_argument('-j', '--jira', required=True, help='JIRA tracker URL') # JIRA tracker URL
-        argumentParser.add_argument('-u', '--username', help='JIRA username') # JIRA username
-        argumentParser.add_argument('-p', '--password', help='JIRA password') # JIRA password
+        argumentParser.add_argument('-j', '--jira', required=True, help='JIRA tracker URL')
+        argumentParser.add_argument('-u', '--username', help='JIRA username')
+        argumentParser.add_argument('-p', '--password', help='JIRA password')
         argumentParser.add_argument('-k', '--keys', help='project keys') # list of project keys separated by comma
-        argumentParser.add_argument('-q', '--queries', help='JQL queries file') # input JQL queries file
+        argumentParser.add_argument('-q', '--queries', help='JQL queries file') # JQL search queries file
         argumentParser.add_argument('-i', '--interval', type=int, default=60, help='update interval') # timer update interval
-        argumentParser.add_argument('-a', '--avatars', default='avatars', help='avatars directory') # avatars directory
-        argumentParser.add_argument('-c', '--icon', default='icon.png', help='icon file') # icon file
+        argumentParser.add_argument('-a', '--avatars', default='jira-avatars', help='avatars directory') # avatars directory
+        argumentParser.add_argument('-c', '--icon', default='jira-notifier.png', help='icon file') # icon file
         arguments = argumentParser.parse_args() # parse command line arguments
 
         self.trackerURL = arguments.jira # get JIRA URL
@@ -221,19 +237,18 @@ class Application: # application logic
         print 'tracker URL: %s' % self.trackerURL
         if self.username:
             print 'username: %s' % self.username
-        print 'project keys: %s' % arguments.queries
-        print 'issue queries file: %s' % os.path.abspath(arguments.queries)
+        print 'project keys: %s' % arguments.keys
         print 'update interval: %d seconds' % self.updateInterval
+        print 'issue queries file: %s' % os.path.abspath(arguments.queries)
         print 'avatars directory: %s' % os.path.abspath(self.avatarsDirectoryName)
         print 'icon file: %s' % os.path.abspath(self.iconFileName)
         print ''
 
         self.initialize()
 
-        print 'starting update loop'
         thread.start_new_thread(self.updateDataLoop, ()) # start new update loop thread
         gobject.threads_init() # allow multiple threads to serialize access to the interpreter
         gtk.main() # run infinite GTK main loop
 
-if __name__ == '__main__': # if it's a main module
-    Application().run() # then construct an Application object and run it
+if __name__ == '__main__': # if it's the main module
+    Application().run() # then construct the Application object and run it
